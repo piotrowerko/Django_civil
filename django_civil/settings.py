8f20b5dc4c709850ea13e99d86b73eb57c8dd6ec
piotrowerko <piotrowerko@outlook.com>
2022-03-07 14:14:33 +0100

    setting DEBUG on basis of enviromental variable

--- django_civil/settings.py
+++ django_civil/settings.py
@@ -137,3 +137,8 @@ LOGIN_URL = 'users:heroku'
 # Ustawienia Heroku:
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
