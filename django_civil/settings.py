5d6703e7a2a99aeacbe01af99c53f41d3ba0bda4
piotrowerko <piotrowerko@outlook.com>
2022-03-14 12:10:33 +0100

    new setting

--- django_civil/settings.py
+++ django_civil/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://django-civil-85.herokuapp.com/rect_sing_reinf']
 
 
 # Application definition
