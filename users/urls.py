321683fd78f5e65e6163bafc08dd575529e6e94c
piotrowerko <piotrowerko@outlook.com>
2022-06-15 11:45:11 +0200

    final refin_ parta

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
+#from rest_framework.authtoken.views import obtain_auth_token  # zmiana systemu logowania na custom
 from .views import(
     registration_view,
     account_properties_view,
 	update_account_view,
+    ObtainAuthTokenView,
 )
 
 app_name = 'users'
 
 urlpatterns = [
     path('register', registration_view, name="register"),
-    path('login', obtain_auth_token, name="login"), # -> see accounts/api/views.py for response and url info
+    #path('login', obtain_auth_token, name="login"), # -> see accounts/api/views.py for response and url info
+    path('login', ObtainAuthTokenView.as_view(), name="login"),
     path('properties', account_properties_view, name="properties"),
 	path('properties/update', update_account_view, name="update"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework.response import Response
 from django.http import JsonResponse
@@ -71,3 +72,32 @@ def update_account_view(request):
 # LOGIN
 # Response: https://gist.github.com/mitchtabian/8e1bde81b3be342853ddfcc45ec0df8a
 # URL: http://127.0.0.1:8000/api/account/login
+
+
+# LOGIN
+# Response: https://gist.github.com/mitchtabian/8e1bde81b3be342853ddfcc45ec0df8a
+# URL: http://127.0.0.1:8000/api/account/login
+class ObtainAuthTokenView(APIView):
+
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request):
+        context = {}
+
+        email = request.POST.get('username')
+        password = request.POST.get('password')
+        account = authenticate(email=email, password=password)
+        if account:
+            try:
+                token = Token.objects.get(user=account)
+            except Token.DoesNotExist:
+                token = Token.objects.create(user=account)
+            context['response'] = 'Successfully authenticated.'
+            context['pk'] = account.pk
+            context['email'] = email.lower()
+            context['token'] = token.key
+        else:
+            context['response'] = 'Error'
+            context['error_message'] = 'Invalid credentials'
+        return Response(context)
